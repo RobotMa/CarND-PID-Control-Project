e0ec38f28f58effcb481040dbf335b9eb50022c3
Qianli Ma <qianli.b.ma@aptiv.com>
2018-07-06 13:37:01 -0400

    better gains

--- src/main.cpp
+++ src/main.cpp
@@ -35,7 +35,7 @@ int main()
   PID pid;
   // TODO: Initialize the pid variable.
   //pid.Init(0.5, 0.5, 0.5);
-  pid.Init(0.11, 0.0, 0.85);
+  pid.Init(0.15, 0.00, 0.85);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
